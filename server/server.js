9b31e409346eb4e58090cb06640dc119ccf50da8
bhatipradeep420 <bhatipradeep420@gmail.com>
2026-07-24 14:53:04 +0530

    Fix Render start command path: add wrapper server.js

---
+++ server/server.js
@@ -0,0 +1,4 @@
+// Wrapper script for Render deployment
+// Render's Start Command is configured to look for server.js in the root of this folder.
+// This simply requires the actual entry point in src/server.js.
+require('./src/server.js');
